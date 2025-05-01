1059b7d03a21089515807b23afbee78655330107
mohammed.bahumaish <mohammed.bahumaish@gmail.com>
2025-05-01 21:33:59 +0300

    refactor

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./dmmfToSchema";
+export * from "./schemaToDmmf";
+export * from "./handleError";
+export * from "@prisma/generator-helper";
+export { type ConfigMetaFormat, formatSchema } from "@prisma/internals";
